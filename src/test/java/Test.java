129b4b54c01ec628810864688c9daaaf98d1f89a
Natasha <chugu@bk.ru>
2019-03-30 02:10:19 +0300

    add

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,26 +2,21 @@ import org.openqa.selenium.By;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
-
+import static org.testng.Assert.assertEquals;
 
 public class Test extends BaseRunner {
 
-    /*@org.junit.Test
+    @org.junit.Test
     public void testEmptyFields(){
         System.out.println("Тест №1");
         driver.get(baseUrl);
 
-        driver.findElement(By.xpath("//*[contains(@class, 'fio-field')]")).click();
-        driver.findElement(By.xpath("//*[contains(@class, 'row_tel')]")).click();
-        driver.findElement(By.xpath("//div[@class='ui-form__wrapper']")).click();
+        clickButton("//*[contains(@class, 'fio-field')]");
+        clickButton("//*[contains(@class, 'row_tel')]");
+        clickButton("//div[@class='ui-form__wrapper']");
 
-        assertEquals("Укажите ваше ФИО",
-                driver.findElement(By.xpath("//*[contains(@class, 'fio-field')]//*[contains(@class, 'error-message')]"))
-                        .getText());
-        assertEquals("Необходимо указать номер телефона",
-                driver.findElement(By.xpath("//*[contains(@class, 'row_tel')]//*[contains(@class, 'error-message')]"))
-                        .getText());
+        getErrorField("Укажите ваше ФИО", "fio-field");
+        getErrorField("Необходимо указать номер телефона", "row_tel");
     }
 
     @org.junit.Test
@@ -34,7 +29,7 @@ public class Test extends BaseRunner {
         driver.findElement(By.cssSelector(".ui-form__wrapper")).click();
         assertEquals("Недостаточно информации. Введите фамилию, имя и отчество через пробел (Например: Иванов Иван Алексеевич)",
                 driver.findElement(By.cssSelector(".ui-form__row_dropdownFIO div[class='ui-form-field-error-message ui-form-field-error-message_ui-form']")).getText());
-        
+
         driver.findElement(By.cssSelector("[name='fio']")).click();
         driver.findElement(By.cssSelector("[name='fio']")).clear();
         driver.findElement(By.cssSelector("[name='fio']")).sendKeys("Наталья# Тес Тес");
@@ -79,16 +74,16 @@ public class Test extends BaseRunner {
         assertEquals("Максимальное количество символов – 50",
                 driver.findElement(By.cssSelector(".ui-form__row_dropdownSuggest div[class='ui-form-field-error-message " +
                         "ui-form-field-error-message_ui-form']:last-child")).getText());
-    }*/
+    }
 
     @org.junit.Test
     public void testGoogle() throws InterruptedException {
         System.out.println("Тест №3");
         driver.get("https://www.google.ru/");
-
-        driver.findElement(By.xpath("//input[@title='Поиск']")).sendKeys("мобайл тинькофф");
-        driver.findElement(By.xpath("//div[@class='sbl1']//span[contains(text(),'мобайл тинькофф')]")).click();
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Тарифы Тинькофф Мобайла'])[1]/following::cite[1]")).click();
+        Thread.sleep(3000);
+        fillingTheField("title", "Поиск", "мобайл тинькофф");
+        clickButton("//div[@class='sbl1']//span[contains(text(),'мобайл тинькофф')]");
+        clickButton("(.//*[normalize-space(text()) and normalize-space(.)='Тарифы Тинькофф Мобайла'])[1]/following::cite[1]");
         Thread.sleep(7000);
         ArrayList<String> tabs2 = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(tabs2.get(1));
@@ -108,13 +103,163 @@ public class Test extends BaseRunner {
     public void testRegion() throws InterruptedException {
         System.out.println("Тест №4");
         driver.get("https://www.tinkoff.ru/mobile-operator/tariffs/");
-        driver.findElement(By.xpath("//div[@class='DOqnW']")).click();
-        driver.findElement(By.xpath("//div[contains(text(),'Москва и Московская обл.')]")).click();
-        Thread.sleep(4000);
+        Thread.sleep(1000);
+        clickButton("//span[@class='MvnoRegionConfirmation__option_v9PfP']");
+        clickButton("//div[@class='MvnoRegionConfirmation__wrapper_1Jmmm MvnoRegionConfirmation__wrapperSelected_uxhv3']");
+        clickButton("//div[contains(text(),'Москва и Московская обл.')]");
+        Thread.sleep(2000);
+
         assertEquals("Москва и Московская область", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Точки продаж'])[1]/following::div[13]")).getText());
         driver.navigate().refresh();
         Thread.sleep(2000);
-        assertEquals("Москва и Московская область", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Точки продаж'])[1]/following::div[13]")).getText());
 
+        assertEquals("Москва и Московская область", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Точки продаж'])[1]/following::div[13]")).getText());
+        String priceMoscow = getPackagePrice();
+        clickButton("//span[contains(text(),'Интернет')]");
+        selectFromList("name","internet",6);
+        //System.out.println(driver.findElement(By.xpath("//select[@name='internet']/following::div[1]/div")).getText());
+        //select[@name='internet']/following::div[1]/div
+        //select[@name='internet']/option[6]
+        clickButton("//span[contains(text(),'Звонки')]");
+        selectFromList("name","calls",6);
+        clickButton("//label[contains(text(),'Режим модема')]");
+        clickButton("//label[contains(text(),'Безлимитные СМС')]");
+
+        String priceFullPackageMoscow = getPackagePrice();
+        clickButton("//div[@class='MvnoRegionConfirmation__wrapper_1Jmmm MvnoRegionConfirmation__wrapperSelected_uxhv3']");
+        clickButton("//div[contains(text(),'Краснодарский кр.')]");
+        Thread.sleep(1000);
+
+        String priceKrasnodar = getPackagePrice();
+        clickButton("//span[contains(text(),'Интернет')]");
+        clickButton("//span[contains(text(),'Безлимитный интернет')]");
+        selectFromList("name","internet",6);
+        clickButton("//span[contains(text(),'Звонки')]");
+        selectFromList("name","calls",6);
+        clickButton("//label[contains(text(),'Режим модема')]");
+        clickButton("//label[contains(text(),'Безлимитные СМС')]");
+
+        String priceFullPackageKrasnodar = getPackagePrice();
+        if(priceMoscow.equals(priceKrasnodar)){
+            System.out.println("Суммы общей цены тарифа с выбранными пакетами и сервисами по дефолту для регионов Москва и Краснодар одинаковые");
+        } else {
+            System.out.println("Суммы общей цены тарифа с выбранными пакетами и сервисами по дефолту для регионов Москва и Краснодар разные");
+        }
+        if(priceFullPackageMoscow.equals(priceFullPackageKrasnodar)){
+            System.out.println("Суммы общей цены тарифа с максимальными выбранными пакетами и сервисами для регионов Москва и Краснодар одинаковые");
+        } else {
+            System.out.println("суммы общей цены тарифа с максимальными выбранными пакетами и сервисами для регионов Москва и Краснодар одинаковые");
+        }
     }
+
+    @org.junit.Test
+    public void notActiveButton() throws InterruptedException {
+
+        System.out.println("Тест №5");
+        driver.get("https://www.tinkoff.ru/mobile-operator/tariffs/");
+
+        clickButton("//span[text() ='Да']");
+        clickButton("//div[@class='MvnoRegionConfirmation__title_DOqnW']");
+        clickButton("//div[contains(text(),'Москва и Московская обл.')]");
+        Thread.sleep(1000);
+
+        clickButton("//span[contains(text(),'Интернет')]");
+        Thread.sleep(1000);
+
+        clickButton("//span[contains(text(),'0 ГБ')]");
+        Thread.sleep(1000);
+
+        clickButton("//span[contains(text(),'Звонки')]");
+        Thread.sleep(1000);
+
+        clickButton("//span[text() = '0 минут']");
+        Thread.sleep(1000);
+
+        clickButton("//label[contains(text(),'Мессенджеры')]");
+        clickButton("//label[contains(text(),'Социальные сети')]");
+        String priceZero = getPackagePrice();
+
+        clickButton("//*[contains(@class, 'fio-field')]");
+        fillingTheField("name","fio", "Тест Тест Тест");
+        clickButton("//*[contains(@class, 'row_tel')]");
+        fillingTheField("name","phone_mobile", "9896868768");
+
+        Thread.sleep(1000);
+        if(Integer.parseInt(priceZero) == 0){
+            clickButton("//div[@class='LoaderRound__container_no-background_GvpfD LoaderRound__container_coverParent_2-_fi']");
+        } else {
+            System.out.println("Цена пакета не равна нулю");
+        }
+        Thread.sleep(1000);
+
+        clickButton("//span[contains(text(),'Заказать доставку')]");
+        Thread.sleep(1000);
+
+        if(driver.findElement(By.xpath("//div[contains(text(),'Укажите район или город')]")) != null){
+            System.out.println("Кнопка активна");
+        }else {
+            System.out.println("Кнопка нективна");
+        }
+    }
+
+    private String getPackagePrice(){
+        return driver.findElement(By.xpath("//h3[contains(text(),'Общая цена:')]"))
+                .getText()
+                .replace("Общая цена: ", "")
+                .replace(" \u20BD","");
+    }
+
+        // Для класса TextInput
+        private void fillingTheField(String nameElement, String nameField, String setValue){
+            driver.findElement(By.xpath("//input[@" + nameElement + "='" + nameField + "']")).sendKeys(setValue);
+        }
+
+        String getValue(String nameElement, String nameField){
+            // Реализовано из-за пункта в дз. В тестах метод пока не применяется
+            return driver.findElement(By.xpath("//[" + nameElement + "='" + nameField + "']")).getAttribute("value");
+        }
+
+        private void getErrorField(String actualResult, String nameField){
+            assertEquals( actualResult,
+                    driver.findElement(By.xpath("//*[contains(@class, '" + nameField + "')]//*[contains(@class, 'error-message')]"))
+                            .getText());
+        }
+
+        // Для класса Button
+        private void clickButton(String path){
+            driver.findElement(By.xpath(path)).click();
+        }
+
+        //Для класса Select
+        void selectFromList(String nameElement, String nameField, Integer numberRecord){
+            //select[@name='internet']/option[6]
+            driver.findElement(By.xpath("//select[@" + nameElement + "='" + nameField + "']/option[" + numberRecord + "]")).getText();
+        }
+
+        String getValueList(String nameElement, String nameField){
+            //получения текущего значения списка
+            //select[@name='internet']/following::div[1]/div/div
+            return driver.findElement(By.xpath("//select[@" + nameElement + "='" + nameField + "']/following::div[1]/div/div")).getText();
+        }
+
+        //  для класса checkBox
+        void setActive(Boolean valueCheckBox){
+
+        }
+
+        String getTextCheckBox(){
+            //CheckboxWithDescription__checkbox_2E0r_
+            return "";
+        }
+
+        String stateCheckBox(String nameCheckBox){
+
+            //Checkbox__container_AZX42 Checkbox__container_checked_3yg5S Checkbox__container_size_l_3He3r
+            //Checkbox__container_AZX42 Checkbox__container_size_l_3He3r
+
+            //Checkbox__container_AZX42 Checkbox__container_size_l_3He3r
+            //Checkbox__container_AZX42 Checkbox__container_checked_3yg5S Checkbox__container_size_l_3He3r вкл
+            return "";
+        }
+
 }
